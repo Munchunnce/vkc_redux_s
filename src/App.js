56e555e0c6573597cbdc2f488ec7d6ba138882e1
Vimal Kumar Chaudhary <112014356+Munchunnce@users.noreply.github.com>
2026-03-13 23:57:22 +0530

    Add Provider and PersistGate to App component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import store, { persistor } from './store/store'; // persistor import
 import { PersistGate } from 'redux-persist/integration/react'; // PersistGate import
 
 
+
 function App() {
   return (
     <Provider store={store}>
