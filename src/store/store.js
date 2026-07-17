d21ed0553970ddf68f5bd6a62a7a4aa36bb31436
Vimal Kumar Chaudhary <112014356+Munchunnce@users.noreply.github.com>
2026-07-17 22:28:45 +0530

    Add imports for Redux Persist in store.js

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './cartSlice';
 import productReducer from './productSlice';
 
+
 // Redux Persist Imports
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage
